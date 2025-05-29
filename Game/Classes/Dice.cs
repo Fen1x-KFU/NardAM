745d7f9dafb3672c5cb7e1e374d9a4cf585d0cde
AngelaPushkina <puskinaanzela06@gmail.com>
2025-05-29 19:06:17 +0300

    Добавьте файлы проекта.

---
+++ Game/Classes/Dice.cs
@@ -0,0 +1,32 @@
+﻿namespace Game
+{
+    internal class Dice
+    {
+        /// <summary>
+        /// Класс с рандомом (для выпадения)
+        /// </summary>
+        public Random random = new Random();
+        /// <summary>
+        /// Значение 1
+        /// </summary>
+        public int Value1 { get; private set; }
+        /// <summary>
+        /// Значение 2
+        /// </summary>
+        public int Value2 { get; private set; }
+        /// <summary>
+        /// Проверка на выпадение одинаковых значений
+        /// </summary>
+        public bool IsDouble => Value1 == Value2;
+        public void Roll()
+        {
+            Value1 = random.Next(1, 7);
+            Value2 = random.Next(1, 7);
+        }
+
+        public void ResettingValues()
+        {
+            Value1 = Value2 = 0;
+        }
+    }
+}
---
+++ Game/Classes/Player.cs
@@ -0,0 +1,34 @@
+﻿namespace Game
+{
+    internal class Player
+    {
+        /// <summary>
+        /// Имя игрока
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Цвет игрока
+        /// </summary>
+        public Color Color { get; set; }
+        /// <summary>
+        /// Сколько шашек дома
+        /// </summary>
+        public int ChipsAtHome { get; set; }
+        /// <summary>
+        /// Ход
+        /// </summary>
+        public bool Move { get; set; }
+        /// <summary>
+        /// Базовый конструктор класса
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="color"></param>
+        public Player(string name, Color color)
+        {
+            Name = name;
+            Color = color;
+            ChipsAtHome = 0;
+            Move = false;
+        }
+    }
+}
